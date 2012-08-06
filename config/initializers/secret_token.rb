ce9fa72d79dc1f5aa110039221f279baf4578397
Jeremy Bertrand <bertrandjeremy@MacBook-Pro-de-Jeremy.local>
2012-08-06 20:13:16 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScanGallery::Application.config.secret_token = 'cb9f0140e9ced764011d4f816dd749f201abdbe400e94d4a0ff92460cadf9a2e58dda67f675340b8eed72d9bcda52bbbbfd69253bb1e82fbabb9d00033f2ed74'

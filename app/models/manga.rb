e67d742ee9f563af05a87e5464031dbb3e221bf0
NotGrm <notgrm@gmail.com>
2012-08-07 07:06:31 +0200

    Add Manga name validation

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -1,3 +1,5 @@
 class Manga < ActiveRecord::Base
   attr_accessible :name
+
+  validates :name, :presence => true
 end
--- test/unit/manga_test.rb
+++ test/unit/manga_test.rb
@@ -4,4 +4,9 @@ class MangaTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save manga without name" do
+  	manga = Manga.new
+  	assert !manga.save, "Saved the manga without a name"
+  end
 end

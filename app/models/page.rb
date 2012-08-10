401d722a4d190b403d6414c6e3fde0ce23b58879
NotGrm <notgrm@gmail.com>
2012-08-10 13:22:47 +0200

    add page navigation

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,4 +6,13 @@ class Page < ActiveRecord::Base
 
   validates :number, :presence => true
   validates_presence_of :image
+
+  def previous_page
+  	Page.find_by_number_and_chapter_id(number - 1, chapter.id)
+  end
+
+  def next_page
+  	Page.find_by_number_and_chapter_id(number + 1, chapter.id)
+  end
+
 end
